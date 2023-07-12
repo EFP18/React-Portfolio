775b0884d1a1ecf8600626e3df934c50441ce19e
EFP18 <ester.p18@hotmail.com>
2023-07-12 09:04:32 -0500

    Added additional projects in portfolio, edited footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   // All functional components must have a return method that contains JSX.
   // We return all the JSX inside a parent element with a className of "container".
   return (
-    <div className=" footer Footer">
-      <h1 className="container"> Thank you! EFP18	&copy; </h1>
-      
+    <div className=" footer ">
+      <h1 className="Footer"> Thank you! EFP18 &copy; </h1>
     </div>
   );
 }
 
-export default Footer
+export default Footer;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,24 +11,27 @@ function Portfolio() {
   const projects = [
     {
       id: 1, 
-      title: "Marvel-Dex",
-      description: "A Pokédex for Marvel Characters",
-      url: "https://dylanstormjohnson.github.io/marvel-dex/",
-      // relative path to image when mapping through
-      image: require("../images/marveldex.png")
-    },
-    {
-      id: 2, 
       title: "Code Trove",
       description: "Code Snippets and more!",
       url: "https://code-trove.herokuapp.com/",
+      githubURL: "https://github.com/EFP18/Code-Trove", 
       image: require("../images/codetrove.png")
     },    
+    {
+      id: 2, 
+      title: "Marvel-Dex",
+      description: "A Pokédex for Marvel Characters",
+      url: "https://dylanstormjohnson.github.io/marvel-dex/",
+      githubURL: "https://github.com/dylanstormjohnson/marvel-dex", 
+      // relative path to image when mapping through
+      image: require("../images/marveldex.png")
+    },
     {
       id: 3, 
       title: "Employee-Tracker",
       description: "Content management system",
       url: "https://github.com/EFP18/Employee-Tracker/tree/main",
+      githubURL: "https://github.com/EFP18/Employee-Tracker",
       image: require("../images/splash2.jpg")
     },
     {
@@ -36,8 +39,25 @@ function Portfolio() {
       title: "J.A.T.E",
       description: "Text Editor with Offline Functionality",
       url: "https://jate-texteditor-efp18-ed59a4bb5c66.herokuapp.com/",
+      githubURL: "https://github.com/EFP18/Text-Editor",
       image: require("../images/jate.png")
     },
+    {
+      id: 5, 
+      title: "Social Network API",
+      description: "Backend Social Network API",
+      url: "https://drive.google.com/file/d/1L763RgP0Pl-fbfj7zFm7gQ0AQSiEiNsm/view",
+      githubURL: "https://github.com/EFP18/Social-Network-API",
+      image: require("../images/socialnetworkapi.png")
+    },
+    {
+      id: 6, 
+      title: "Weather Dashboard",
+      description: "Weather Dashboard",
+      url: "https://efp18.github.io/Weather-Dashboard/",
+      githubURL: "https://github.com/EFP18/Weather-Dashboard",
+      image: require("../images/weatherdash.png")
+    }
   ]
   return (
     <Page title={'Portfolio'}>
