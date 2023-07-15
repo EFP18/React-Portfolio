ae775d20a7d50122b0e2ea0dba80a83b882972f9
EFP18 <ester.p18@hotmail.com>
2023-07-15 09:40:43 -0500

    edited resume, added react bootstrap

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import resume from '../images/resume.pdf'
-import Page from './Page';
+import React from "react";
+import resume from "../images/resume.pdf";
+import Page from "./Page";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import { Link } from "react-router-dom";
 
 function Resume() {
-
   // All functional components must have a return method that contains JSX.
   // We return all the JSX inside a parent element with a className of "container".
   return (
-    <Page title={'Resume'}>
-      <div className="card skills">
-        {/* <h5 className="card-header skills">Skills & Education</h5> */}
-        <div className="card-body skills">
-          <h5 className="card-title">Skills</h5>
-          <p className="card-text">Languages: HTML, CSS, JAVASCRIPT</p>
-          <p className="card-text">Applications: Bootstrap, jQuery, Node.js, GitHub, Express.js, React.js, SQL</p>
-          <a href={resume} download className="btn btn-primary">Click here to download my resume!</a>
-          <div className="education">
-            <h5 className="card-title ">Education</h5>
-            <p className="card-text">Languages: HTML, CSS, JAVASCRIPT</p>
-            <p className="card-text">Applications: Bootstrap, jQuery, Node.js, GitHub, Express.js, React.js, SQL</p>
-          </div>
-
-        </div>
-      </div>
+    <Page title={"Resume"}>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Skills</h1>
+              <p>Languages: HTML, CSS, JAVASCRIPT</p>
+              <p>
+                Applications: Bootstrap, jQuery, Node.js, GitHub, Express.js,
+                React.js, SQL
+              </p>
+            </Col>
+            <Col>
+              <h1>Education</h1>
+              <p>Vanderbilt: Full Stack Web Development</p>
+              <p>
+                University of Groningen: Linguistics - European Languages and
+                Cultures
+              </p>
+            </Col>
+            <a href={resume} download className="btn btn-primary">
+              Click here to download my resume!
+            </a>
+          </Row>
+        </Container>
     </Page>
   );
 }
