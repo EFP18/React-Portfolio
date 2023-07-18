c73244ba8b7ff1533acb801bd56f890f64d3e828
EFP18 <ester.p18@hotmail.com>
2023-07-18 15:46:27 -0500

    corrected side scroll

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import "../src/styles/App.css";
+import '../src/styles/App.css';
 // import all the components
-import ContactForm from "./components/ContactForm";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import AboutMe from "./components/AboutMe";
-import Portfolio from "./components/Portfolio";
-import Resume from "./components/Resume";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import `useEffect` Hook from React
-// import { useEffect } from "react";
+import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       {/* anything between the browserrouter tag is react router dom functionality */}
       <BrowserRouter>
         <Header></Header>
@@ -21,10 +18,10 @@ function App() {
         <Routes>
           {/* when on localhost:3000 / route, only display the Home page */}
           {/* endpoints */}
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/Portfolio" element={<Portfolio />} />
-          <Route path="/Resume" element={<Resume />} />
-          <Route path="/Contact" element={<ContactForm />} />
+          <Route path='/' element={<AboutMe />} />
+          <Route path='/Portfolio' element={<Portfolio />} />
+          <Route path='/Resume' element={<Resume />} />
+          <Route path='/Contact' element={<ContactForm />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,60 +4,62 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Page from './Page';
 import pictureSp from '../images/splash2.jpg';
 import headShot from '../images/headshotPortfolio.png';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 function AboutMe() {
   // All functional components must have a return method that contains JSX.
   // We return all the JSX inside a parent element with a className of "container".
   return (
-    <Page title={'Ester Pelosof - Home'}>
-      {/* create a row in the page that will have 2 columns, one for the headshot and one for the bio */}
-      <Row>
-        <img
-          className='imageAboutMe'
-          src={pictureSp}
-          alt='aboutMeImage'
-          style={{ paddingBottom: '50px', width: '100%' }}
-        />
-
-        <Col sm={2}>
+    <Page title={'Ester Pelosof - Home'} className=''>
+      <Container>
+        {/* create a row in the page that will have 2 columns, one for the headshot and one for the bio */}
+        <Row>
           <img
-            src={headShot}
-            className='rounded-circle'
-            alt='headshot'
-            style={{ height: '150px', marginLeft: '50px', width: '110px' }}
+            className='imageAboutMe'
+            src={pictureSp}
+            alt='aboutMeImage'
+            style={{ paddingBottom: '50px', width: '100%' }}
           />
-        </Col>
-        <Col sm={10}>
-          <div className='mb-3 ml-4'>
-            <div className='card-body aboutMeText'>
-              <h5 className='card-title cardTitle mb-4'>
-                Hey! My name is Ester!
-              </h5>
-              <p className='card-text aboutMeText'>
-                Very methodical and hard working, I enjoy solving puzzles and
-                answering complex questions. I am very passionate about
-                languages and I'm very interested in learning some programming
-                languages as well, to combine them with creativity. I am very
-                eager to learn, well-organized, and I work very well with
-                deadlines.
-              </p>
-              <p className='card-text aboutMeText'>
-                I studied Linguistics at the University of Groningen in the
-                Netherlands. I'm really passionate about studying different
-                languages, and I find the creation of different structures and
-                sentences very interesting, which is why I decided to study
-                programming languages and coding.
-              </p>
-              <p className='card-text'>
-                <small className='text-muted smallMsg'>
-                  Contact me for more information!!
-                </small>
-              </p>
+
+          <Col sm={2}>
+            <img
+              src={headShot}
+              className='rounded-circle'
+              alt='headshot'
+              style={{ height: '150px', marginLeft: '50px', width: '110px' }}
+            />
+          </Col>
+          <Col sm={10}>
+            <div className='mb-3 ml-4'>
+              <div className='card-body aboutMeText'>
+                <h5 className='card-title cardTitle mb-4'>
+                  Hey! My name is Ester!
+                </h5>
+                <p className='card-text aboutMeText'>
+                  Very methodical and hard working, I enjoy solving puzzles and
+                  answering complex questions. I am very passionate about
+                  languages and I'm very interested in learning some programming
+                  languages as well, to combine them with creativity. I am very
+                  eager to learn, well-organized, and I work very well with
+                  deadlines.
+                </p>
+                <p className='card-text aboutMeText'>
+                  I studied Linguistics at the University of Groningen in the
+                  Netherlands. I'm really passionate about studying different
+                  languages, and I find the creation of different structures and
+                  sentences very interesting, which is why I decided to study
+                  programming languages and coding.
+                </p>
+                <p className='card-text'>
+                  <small className='text-muted smallMsg'>
+                    Contact me for more information!!
+                  </small>
+                </p>
+              </div>
             </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Container>
     </Page>
   );
 }
